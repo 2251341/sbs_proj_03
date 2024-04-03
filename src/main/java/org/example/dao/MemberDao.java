3bdc250342594cffaa7c000ad415039bd3621cf4
2251341 <dydwksos246@naver.com>
2024-04-03 23:55:49 +0900

    회원 서비스 도입

--- src/main/java/org/example/dao/MemberDao.java
+++ src/main/java/org/example/dao/MemberDao.java
@@ -11,4 +11,32 @@ public class MemberDao extends Dao {
     public MemberDao() {
         members = new ArrayList<>();
     }
+
+    public void join(Member member) {
+        members.add(member);
+        lastId++;
+    }
+
+    public int getMemberIndexByLoginId(String loginId) {
+        int i = 0;
+
+        for ( Member member : members ) {
+            if ( member.loginId.equals(loginId) ) {
+                return i;
+            }
+            i++;
+        }
+
+        return -1;
+    }
+
+    public Member getMemberByLoginId(String loginId) {
+        int index = getMemberIndexByLoginId(loginId);
+
+        if ( index == -1 ) {
+            return null;
+        }
+
+        return members.get(index);
+    }
 }
