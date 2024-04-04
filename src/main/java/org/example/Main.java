9a72bbe2829c2807f01578d1c83b100a73d8c3f9
2251341 <dydwksos246@naver.com>
2024-04-04 15:58:12 +0900

    게시판에 따른 리스트 정렬, 검색어 기능 유지'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,5 @@
 package org.example;
 
-import org.example.dto.Article;
-import org.example.util.Util;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
